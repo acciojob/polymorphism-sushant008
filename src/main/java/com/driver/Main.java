6e5c09a2f0276b89251b24ad861768a31a5e7b8c
Your Name <you@example.com>
2023-05-22 08:53:11 +0530

    Second

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,30 @@
 package com.driver;
 
 public class Main {
+    public static class  Product{
+
+        public double product(double a, double b) {
+            return a*b;
+        }
+
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+
+        public int product(int x, int y) {
+            return x*y;
+        }
+
+//        public Product() {
+//        }
+    }
+
+    public static void main(String[] args) {
+
+        Product p = new Product();
+        System.out.println(p.product(2,5));
+        System.out.println(p.product(2,5,9));
+        System.out.println(p.product(2.0,5.0));
+    }
 
 }
